599ba3655cf57cf1654ab0f7647bc707980a99d9
AlbyMiscy <albymiscy07@outlook.it>
2025-08-13 21:47:55 +0200

    step04 - modifica gestione frutta per livello successivo: diminuita la soglia per arrivare al livello successivo, invece che 10 ho messo 8

--- step04/functionHandler/engineHandler/update.cpp
+++ step04/functionHandler/engineHandler/update.cpp
@@ -80,7 +80,7 @@ void Engine::update(){
             updateTextContent(); // Update text
 
             bool beginningNewLevel = false;
-            if(fruitEatenThisLevel >= 10){
+            if(fruitEatenThisLevel >= 8){
                 // Check if there are more levels
                 if(currentLevel < maxLevels){
                     beginningNewLevel = true;
